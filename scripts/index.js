9e138fe6faf7517e57cb4ff4348ff7c23b76554c
WhitneySt <whitneystena418@gmail.com>
2024-06-28 12:18:11 -0500

    Agregando funcionalidad de filtrados de videos por categorías, búsqueda de videos por búsqueda

--- scripts/index.js
+++ scripts/index.js
@@ -1,46 +1,53 @@
 import { videos } from "../modules/data.js";
+import {
+  pintarVideos,
+  agregarBotonesDeFiltrado,
+  ejecutarFiltros,
+} from "../modules/ui.js";
 
-console.log(videos);
-
-console.log(JSON.stringify(videos));
-
-//Variables/Constantes
-
-//funciones
-
-function agregarBotonesDeFiltrado(listaCategorias, contenedorFiltros) {
-  listaCategorias.forEach((element) => {
-    const boton = document.createElement("button");
-    boton.id = element;
-    boton.textContent = element;
-    // item.innerHTML = `
-    //     <button id=${element} name=${element}>${element}</button>
-    // `;
-    boton.addEventListener("click", () => {
-      console.log(`Hice click en el filtro ${element}`);
-      mostrarFiltroActivo(contenedorFiltros.children, element);
-    });
-    contenedorFiltros.appendChild(boton);
-    console.log(contenedorFiltros.children);
-  });
-}
-
-function mostrarFiltroActivo(botonesFiltrado, filtroAActivar) {
-  for (const boton of botonesFiltrado) {
-    boton.classList.remove("active");
-    if (boton.id === filtroAActivar) {
-      boton.classList.add("active");
-    }
-  }
-}
-
-//1. Agregar en el DOM los botones de filtrado
 const contenedorFiltros = document.getElementById("filtros");
-
 const listaCategorias = videos.map((video) => video.categoria);
 // const categorias = [...new Set(listaCategorias)]; //Spread operators
 const categorias = Array.from(new Set(listaCategorias));
+const contenedorVideos = document.querySelector("main");
 
-agregarBotonesDeFiltrado(categorias, contenedorFiltros);
+//1. Agregar en el DOM los botones de filtrado
+agregarBotonesDeFiltrado(
+  categorias,
+  contenedorFiltros,
+  videos,
+  contenedorVideos
+);
 
 //2. Agragar las tarjetas de los videos en el DOM
+pintarVideos(videos, contenedorVideos);
+
+//3. Funcionalidad al boton todos
+const botonTodos = document.getElementById("todos");
+botonTodos.addEventListener("click", (evento) => {
+  ejecutarFiltros(
+    evento.target.id,
+    contenedorFiltros.children,
+    videos,
+    contenedorVideos
+  );
+});
+
+//4. Funcionalidad de búsqueda de video por nombre
+const formularioBusqueda = document.getElementById("busqueda");
+
+formularioBusqueda.addEventListener("submit", (evento) => {
+  evento.preventDefault();
+  const inputBusqueda = document.getElementById("inputBusqueda");
+  const terminoBusqueda = inputBusqueda.value;
+  if (terminoBusqueda.trim() !== "") {
+    const resultadoBusqueda = busquedaVideoPorNombre(videos, terminoBusqueda);
+    pintarVideos(resultadoBusqueda, contenedorVideos);
+  }
+});
+
+function busquedaVideoPorNombre(listaVideos, nombreABuscar) {
+  return listaVideos.filter((video) =>
+    video.namevideo.toLowerCase().includes(nombreABuscar.toLowerCase())
+  );
+}
